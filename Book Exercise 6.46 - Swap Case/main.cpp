923ffd5fa2a60d9eb3879f06b0e2c7069eeb2dce
alexberry721 <alexberry721@gmail.com>
2016-10-03 21:00:15 -0700

    Initial Commit

---
+++ Book Exercise 6.46 - Swap Case/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Book Exercise 6.46 - Swap Case
+//
+//  Created by ax on 10/3/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
